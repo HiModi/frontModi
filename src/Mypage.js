f009f18e4e3295ccab0295c765e07845d670e3bb
ovommmHJ <killos033@gmail.com>
2023-08-21 23:36:27 +0900

    230821 axios 테스트 진행중

--- src/Mypage.js
+++ src/Mypage.js
@@ -1,16 +1,23 @@
 import { useNavigate } from "react-router-dom";
 import React from "react";
-
+import axios from "axios";
 const Mypage = () =>{
     const movePage = useNavigate();
     function gohomepage(){
         movePage('/');
     }
 
+    function getValue1(){
+        axios.get("http://mo-di.kro.kr:90/value2")
+            .then((Response)=>{console.log(Response.data)})
+            .catch((Error)=>{console.log(Error)})
+        }
     return (
         <div className="Mypage">
             Mypage
             Second!!!!!!!
+
+            <button onClick={getValue1}>값 가져와</button>
             <button onClick={gohomepage}>홈화면 이동</button>
         </div>
     );
